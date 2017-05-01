a2b4474b0865a399cbc2f76cc58af4ea54ca5b9d
Vinay Sudhir Shah <vinay.sudhir@gmail.com>
2017-05-01 23:29:49 +0530

    Barclay epode Payments

--- index.php
+++ index.php
@@ -1,18 +1,18 @@
 <?php
 /*
-Plugin Name: CB BARCLAY EPDQ
-Description: woocommerce epdq
-Plugin URI: http://#
+Plugin Name: Catchtheweb BARCLAY ePDQ Payment Gateway
+Description: woocommerce epdq payment gateway
+Plugin URI: http://catchtheweb.net/
 Author: Vinay Shah
 Author URI: http://vinayshah.in
 Version: 2.1
 License: GPL2
-Text Domain: cb-barclay
+Text Domain: vc-barclay
 */
 
 if( ! defined( 'ABSPATH' ) ) exit;
 
-require 'classes/cb_barclay_payment.php';
+require 'classes/vc_barclay_payment.php';
 
-global $cb_barclay_payment;
-$cb_barclay_payment = new CB_Barclay_Payment( __FILE__ );
+global $vc_barclay_payment;
+$vc_barclay_payment = new VC_Barclay_Payment( __FILE__ );
